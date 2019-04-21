fe7c9cc53c7c73bc3e74510587d5232923bccfb2
Asker <au3wk4@gmail.com>
2019-04-21 22:43:31 +0800

    wcf example

--- Wcf_Service/IWeather.cs
+++ Wcf_Service/IWeather.cs
@@ -9,7 +9,7 @@ namespace Wcf_Service
 {
 	// 注意: 您可以使用 [重構] 功能表上的 [重新命名] 命令同時變更程式碼和組態檔中的介面名稱 "IService1"。
 	[ServiceContract]
-	public interface IService1
+	public interface IWeather
 	{
 		[OperationContract]
 		string GetData(int value);
---
+++ host_wcf_winform/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace host_wcf_winform
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ServiceHost host = new ServiceHost(typeof(Wcf_Service.Weather));
+
+			host.Open();
+			MessageBox.Show("Service Started");
+
+		}
+	}
+}
